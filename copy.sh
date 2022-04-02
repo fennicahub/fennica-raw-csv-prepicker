597649310ba81394aac6540687708564f0e2ff92
antagomir <leo.lahti@iki.fi>
2022-04-02 18:55:02 +0300

    Tested the workflow, all goes well; added new cfg files

---
+++ copy.sh
@@ -0,0 +1,5 @@
+cp out/estc_raw_duplicated.csv estc-data-verified/estc-csv-raw-filtered/
+cp out/estc_raw_bad.csv estc-data-verified/estc-csv-raw-filtered/
+cp out/estc_raw_sane.csv estc-data-verified/estc-csv-raw-filtered/
+cp out/record_seq_estc_id_pairs.csv estc-data-verified/estc-csv-raw-filtered/
+
